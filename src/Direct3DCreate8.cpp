765bad70ce28a32efe549d1361b4c727324182e0
actboy168 <actboy168@gmail.com>
2014-03-24 16:06:45 +0800

    动态加载d3d9.dll

--- src/Direct3DCreate8.cpp
+++ src/Direct3DCreate8.cpp
@@ -1,11 +1,29 @@
 #include "Direct3D8.h"
 
-#pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "dxguid.lib")
+
+typedef IDirect3D9* (__stdcall* FnDirect3DCreate9)(UINT);
+
 
 extern "C"
 IDirect3D8* __stdcall Direct3DCreate8(UINT /*sdk_version*/)
 {
+	HMODULE lib = GetModuleHandleW(L"d3d9.dll");
+	if (!lib)
+	{
+		lib = LoadLibraryW(L"d3d9.dll");
+		if (!lib)
+		{
+			return NULL;
+		}
+	}
+
+	FnDirect3DCreate9 Direct3DCreate9 = (FnDirect3DCreate9)GetProcAddress(lib, "Direct3DCreate9");
+	if (!Direct3DCreate9)
+	{
+		return NULL;
+	}
+
 	IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
-	return d3d ? new CDirect3D8(d3d) : NULL;
+	return d3d ? new CDirect3D8(d3d): NULL;
 }
